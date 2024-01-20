90bbbef250151e832b879fe71bcead12d65ee7d6
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-20 20:08:22 +0800

    Add the instructions and pseudocode

---
+++ product_and_sum_of_two_numbers.py
@@ -0,0 +1,11 @@
+# Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
+
+# pseudocode
+
+# ask user for two numbers
+
+# compute product of two numbers
+
+# if product of two numbers is less than or equal to 1000, return product
+
+# if product is greater than 1000, compute sum of two numbers
