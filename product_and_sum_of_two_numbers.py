a5c13a8558fe0dfd7e5bacc941ea06b5acad5aa8
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-20 20:31:26 +0800

    Refactor product_and_sum_of_two_numbers function to include descriptive return messages

--- product_and_sum_of_two_numbers.py
+++ product_and_sum_of_two_numbers.py
@@ -9,11 +9,19 @@ def multiply_and_sum(num_1, num_2):
 
     # if product of two numbers is less than or equal to 1000, return product
     if product <= 1000:
-        return product
+        return (
+            "The product of the two numbers is equal or lower than 1000. The product is "
+            + str(product)
+            + "."
+        )
     # if product is greater than 1000, compute sum of two numbers
     else:
         sum = num_1 + num_2
-        return sum
+        return (
+            "The product of the two numbers is greater than 1000. The sum is "
+            + str(sum)
+            + "."
+        )
 
 
 # ask user for two numbers
