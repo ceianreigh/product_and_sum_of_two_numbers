a5fd0a28ee403636bd0645a6d04cb43357623dc5
Ceianreigh Keann Cepillo <ceianreighkeannmcepillo@iskolarngbayan.pup.edu.ph>
2024-01-20 20:19:48 +0800

    Move some pseudocode upward and made the multiply_and_sum function

--- product_and_sum_of_two_numbers.py
+++ product_and_sum_of_two_numbers.py
@@ -2,14 +2,18 @@
 
 # pseudocode
 
+
+# compute product of two numbers
+def multiply_and_sum(num_1, num_2):
+    product = num_1 * num_2
+    return product
+    # if product of two numbers is less than or equal to 1000, return product
+
+    # if product is greater than 1000, compute sum of two numbers
+
+
 # ask user for two numbers
 num_1 = int(input("Enter first number: "))
 num_2 = int(input("Enter second number: "))
 result = multiply_and_sum(num_1, num_2)
 print(result)
-
-# compute product of two numbers
-
-# if product of two numbers is less than or equal to 1000, return product
-
-# if product is greater than 1000, compute sum of two numbers
